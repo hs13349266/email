35feeb655959075fd9d64f0fcfbb24592b6404cd
Barry <Barry.he@mso-china.com>
2019-04-04 15:53:58 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.get('/', function (req, res, next) {
 
 router.post('/sendEmail', async (req, res, next) => {
     try {
+        //基本配置
         let transporter = nodeMailer.createTransport({
             host: config.host,
             port: config.port,
@@ -18,22 +19,24 @@ router.post('/sendEmail', async (req, res, next) => {
                 pass: config.pass
             }
         });
-        console.log(JSON.stringify(req.body));
-        // setup email data with unicode symbols
-        let mailOptions = JSON.parse(req.body.mailOptions);
-        if (!mailOptions) {
-            res.status(500).send({err: "缺失请求参数"});
+        console.log('请求参数:', JSON.stringify(req.body));
+
+        //判断请求体中是否存在发送邮件的必要参数,不存在直接返回错误
+        if (!req.body.mailOptions) {
+            res.status(500).send({errMsg: "缺失邮件配置参数"});
             return;
         }
-        // send mail with defined transport object
+        let mailOptions = JSON.parse(req.body.mailOptions);
+
+        //调用发送邮件的方法
         let info = await transporter.sendMail(mailOptions);
 
-        console.log("Message sent: %s", info.messageId);
-        // Preview only available when sending through an Ethereal account
-        //console.log("Preview URL: %s", nodeMailer.getTestMessageUrl(info));
+        console.log('邮件服务器返回信息:', info);
+
         res.status(200).send({data: info});
     } catch (e) {
-        res.status(500).send({err: e});
+        console.error(new Date().toString() + '  :  ',e);
+        res.status(500).send({errMsg: "服务器错误!"});
     }
 });
 
