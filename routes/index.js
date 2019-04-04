8a918ed8eabafb9960cd4782889ed1b7d8dd3f5a
Barry <Barry.he@mso-china.com>
2019-04-04 13:37:55 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -4,34 +4,37 @@ const nodeMailer = require("nodemailer");
 const config = require('../config');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
 router.post('/sendEmail', async (req, res, next) => {
-   try {
-       let transporter = nodeMailer.createTransport({
-           host: config.host,
-           port: config.port,
-           auth: {
-               user: config.user,
-               pass: config.pass
-           }
-       });
+    try {
+        let transporter = nodeMailer.createTransport({
+            host: config.host,
+            port: config.port,
+            auth: {
+                user: config.user,
+                pass: config.pass
+            }
+        });
 
-       // setup email data with unicode symbols
-       let mailOptions = req.body.mailOptions;
+        // setup email data with unicode symbols
+        let mailOptions = req.body.mailOptions;
+        if (!mailOptions) {
+            res.status(500).send({err: "缺失请求参数"});
+            return;
+        }
+        // send mail with defined transport object
+        let info = await transporter.sendMail(mailOptions);
 
-       // send mail with defined transport object
-       let info = await transporter.sendMail(mailOptions);
-
-       console.log("Message sent: %s", info.messageId);
-       // Preview only available when sending through an Ethereal account
-       //console.log("Preview URL: %s", nodeMailer.getTestMessageUrl(info));
-       res.status(200).send({data:info});
-   } catch (e) {
-       res.status(500).send({err:e});
-   }
+        console.log("Message sent: %s", info.messageId);
+        // Preview only available when sending through an Ethereal account
+        //console.log("Preview URL: %s", nodeMailer.getTestMessageUrl(info));
+        res.status(200).send({data: info});
+    } catch (e) {
+        res.status(500).send({err: e});
+    }
 });
 
 module.exports = router;
